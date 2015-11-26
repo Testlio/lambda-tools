080808db42b1fd21923b4919af17d32164145407
Henri Normak <henri.normak@gmail.com>
2015-11-26 13:17:03 +0200

    Remove synchronize, use async across steps instead

--- bin/steps/process-lambdas.js
+++ bin/steps/process-lambdas.js
@@ -16,100 +16,93 @@ const browserify    = require('browserify');
 const babelify      = require('babelify');
 const presets       = require('babel-preset-es2015');
 const uglify        = require('uglify-js');
-const sync          = require('synchronize');
 const dot           = require('dot');
 const Zip           = require('node-zip');
+const async         = require('async');
 
-module.exports = function(program, configuration, callback) {
-    let stackCF;
-    let lambdaCF;
-    let lambdaOutputTemplate;
+//
+//  Processing helper, results in an object that can be used to
+//  further flesh out the CF stack
+//
+//  { name: ..., output: ..., resource: ... }
+//
+function processLambda(program, configuration, baseResource, outputTemplate, lambdaPath, callback) {
+    let name = path.basename(lambdaPath);
+    let camelName = name.camelCase();
+    camelName = camelName.charAt(0).toLowerCase() + camelName.substring(1);
+    console.log(`Processing "${name}" (${camelName})`);
 
-    try {
-        stackCF = fsx.readJSONFileSync(configuration.cloudFormation.deployment);
-        lambdaCF = fsx.readJSONFileSync(path.join(configuration.directories.working, 'templates/lambda.cf.json'));
-        lambdaOutputTemplate = dot.template(fs.readFileSync(path.join(configuration.directories.working, 'templates/lambda.resource.dot'), 'utf8'));
-    } catch (error) {
-        callback(error, null);
-        return;
+    // Load in the cloudformation config (if any)
+    let lambdaCustomCF = path.join(lambdaPath, 'cf.json');
+    let config = baseResource;
+
+    if (fsx.fileExists(lambdaCustomCF)) {
+        config = merge(baseResource, fsx.readJSONFileSync(lambdaCustomCF));
     }
 
-    let lambdas = fsx.getDirectories(configuration.directories.lambdas);
-    for (let lambda of lambdas) {
-        let name = path.basename(lambda);
-        let camelName = name.camelCase();
-        camelName = camelName.charAt(0).toLowerCase() + camelName.substring(1);
-        console.log(`Processing "${name}" (${camelName})`);
+    let handler = config["Properties"]["Handler"].split('.', 1) + '';
 
-        // Load in the cloudformation config (if any)
-        let lambdaCustomCF = path.join(lambda, 'cf.json');
-        let config = lambdaCF;
+    // Build the result object for this Lambda
+    config["Properties"]["Code"] = {
+        "S3Bucket": configuration.bucket.name,
+        "S3Key": configuration.bucket.directory + '/' + name + '.zip'
+    };
 
-        if (fsx.fileExists(lambdaCustomCF)) {
-            config = merge(lambdaCF, fsx.readJSONFileSync(lambdaCustomCF));
-        }
+    let result = {
+        name: camelName,
+        output: JSON.parse(outputTemplate({name: camelName})),
+        resource: config
+    };
 
-        let handler = config["Properties"]["Handler"].split('.', 1) + '';
+    // Compress and zip the code
+    if (!program.dryRun && !program.skipStack) {
+        let handlerFileName = handler + '.js';
+        console.log("\tEntry point", handlerFileName);
+        let handlerFile = path.join(lambdaPath, handlerFileName);
+        let bundledFile = path.join(configuration.directories.staging, handler + '.bundled.js');
 
-        // Add entry to the CF configuration
-        config["Properties"]["Code"] = {
-            "S3Bucket": configuration.bucket.name,
-            "S3Key": configuration.bucket.directory + '/' + name + '.zip'
-        };
-
-        stackCF["Resources"][camelName] = config;
-        let outputname = 'l' + camelName.charAt(0).toUpperCase() + camelName.substring(1);
-        stackCF["Outputs"][outputname] = JSON.parse(lambdaOutputTemplate({name: camelName}));
-
-        // Compress and zip the code
-        if (!program.dryRun && !program.skipStack) {
-            let handlerFileName = handler + '.js';
-            console.log("\tEntry point", handlerFileName);
-            let handlerFile = path.join(lambda, handlerFileName);
-            let bundledFile = path.join(configuration.directories.staging, handler + '.bundled.js');
-
-            let bundler = new browserify(handlerFile, {
-                basedir: lambda,
-                standalone: handler,
-                browserField: false,
-                builtins: false,
-                commondir: false,
-                ignoreMissing: true,
-                detectGlobals: true,
-                insertGlobalVars: {
-                    process: function() {}
-                }
-            });
-
-            bundler.exclude('aws-sdk');
-            bundler.transform(babelify, {
-                presets: [presets],
-                compact: false,
-                global: true,
-                ignore: /\/node_modules\/(lambda_deployment|\.bin)\/.*/
-            });
-
-            // Build the ZIP file we need
-            let zip = new Zip();
-            zip.file('.env', program.flatEnvironment);
-
-            try {
-                console.log('\tBrowserifying');
-                let bundled = sync.await(bundler.bundle(sync.defer()));
-                fs.writeFileSync(bundledFile, bundled);
-
-                console.log('\tMinifying');
-                let minified = uglify.minify(bundledFile, {
-                    mangle: false,
-                    compress: {}
-                }).code;
-
-                fs.unlinkSync(bundledFile);
-                zip.file(handlerFileName, minified);
-            } catch (error) {
-                console.error("\tFailed to compress code".red, error);
-                process.exit(5);
+        let bundler = new browserify(handlerFile, {
+            basedir: lambdaPath,
+            standalone: handler,
+            browserField: false,
+            builtins: false,
+            commondir: false,
+            ignoreMissing: true,
+            detectGlobals: true,
+            insertGlobalVars: {
+                process: function() {}
             }
+        });
+
+        bundler.exclude('aws-sdk');
+        bundler.transform(babelify, {
+            presets: [presets],
+            compact: false,
+            global: true,
+            ignore: /\/node_modules\/(lambda_deployment|\.bin)\/.*/
+        });
+
+        // Build the ZIP file we need
+        let zip = new Zip();
+        zip.file('.env', program.flatEnvironment);
+
+        console.log('\tBrowserifying');
+        bundler.bundle(function(err, bundled) {
+            if (err) {
+                console.error("\tFailed to compress code".red, error);
+                return callback(err);
+            }
+
+            fs.writeFileSync(bundledFile, bundled);
+
+            console.log('\tMinifying');
+            let minified = uglify.minify(bundledFile, {
+                mangle: false,
+                compress: {}
+            }).code;
+
+            fs.unlinkSync(bundledFile);
+            zip.file(handlerFileName, minified);
 
             const zippedData = zip.generate({
                 type: 'nodebuffer',
@@ -117,12 +110,47 @@ module.exports = function(program, configuration, callback) {
             });
 
             fs.writeFileSync(path.join(configuration.directories.staging, name + '.zip'), zippedData);
-        }
+            console.log('\tDone\n'.green);
 
-        console.log('\tDone\n'.green);
+            callback(null, result);
+        });
+    } else {
+        callback(null, result);
+    }
+}
+
+module.exports = function(program, configuration, callback) {
+    let stackCF;
+    let lambdaCF;
+    let lambdaOutputTemplate;
+
+    try {
+        lambdaOutputTemplate = dot.template(fs.readFileSync(path.join(configuration.directories.working, 'templates/lambda.resource.dot'), 'utf8'));
+        stackCF = fsx.readJSONFileSync(configuration.cloudFormation.deployment);
+        lambdaCF = fsx.readJSONFileSync(path.join(configuration.directories.working, 'templates/lambda.cf.json'));
+    } catch (error) {
+        callback(error, null);
+        return;
     }
 
-    // Store the final CF file along with the zip files
-    fs.writeFileSync(configuration.cloudFormation.deployment, JSON.stringify(stackCF));
-    callback(null, null);
+    let lambdas = fsx.getDirectories(configuration.directories.lambdas).map(function(lambda) {
+        return function(callback) {
+            processLambda(program, configuration, lambdaCF, lambdaOutputTemplate, lambda, callback);
+        };
+    });
+
+    async.series(lambdas, function(err, results) {
+        if (err) return callback(err);
+
+        for (let result of results) {
+            stackCF["Resources"][result.name] = result.resource;
+
+            let outputName = 'l' + result.name.charAt(0).toUpperCase() + result.name.substring(1);
+            stackCF["Outputs"][outputName] = result.output;
+        }
+
+        // Store the final CF file along with the zip files
+        fs.writeFileSync(configuration.cloudFormation.deployment, JSON.stringify(stackCF));
+        callback();
+    });
 };
--- bin/steps/setup.js
+++ bin/steps/setup.js
@@ -8,7 +8,7 @@ const fsx           = require('../helpers/fs_additions');
 const path          = require('path');
 const cp            = require('child_process');
 const fs            = require('fs');
-const sync          = require('synchronize');
+const async         = require('async');
 const AWS           = require('aws-sdk');
 
 function checkDependencies() {
@@ -28,112 +28,123 @@ function checkDependencies() {
 }
 
 module.exports = function(program, workingDirectory, callback) {
-    sync.fiber(function() {
-        // Check dependencies
-        try {
-            checkDependencies();
-        } catch (error) {
-            console.error(error);
-            process.exit(1);
-        }
+    let root = path.resolve(process.cwd());
+    let lambdasDirectory = path.resolve(root, 'lambdas');
+    let lambdasBucket = `lambdas-${program.projectName}-${program.stage}`;
+    let timestamp = Math.floor(new Date() / 1000);
+    let lambdasBucketDirectory = '' + timestamp;
+    let stagingDirectory = path.resolve(workingDirectory, 'lambda_stage');
+    let deploymentFormation = path.join(stagingDirectory, 'deployment.cf.json');
+    let deploymentAPI = path.join(stagingDirectory, 'deployment.api.json');
 
-        // Gather our bearings
-        let root = path.resolve(process.cwd());
-        let lambdasDirectory = path.resolve(root, 'lambdas');
-        let lambdasBucket = `lambdas-${program.projectName}-${program.stage}`;
-        let timestamp = Math.floor(new Date() / 1000);
-        let lambdasBucketDirectory = '' + timestamp;
-        let stagingDirectory = path.resolve(workingDirectory, 'lambda_stage');
-        let deploymentFormation = path.join(stagingDirectory, 'deployment.cf.json');
-        let deploymentAPI = path.join(stagingDirectory, 'deployment.api.json');
-
-        // Make sure there are Lambda functions, otherwise abort
-        if (!fsx.directoryExists(lambdasDirectory)) {
-            console.error("Lambdas directory does not exist".red);
-            process.exit(2);
-        }
-
-        // Create the staging directory (wipe any old one)
-        try {
-            fsx.recreateDirectory(stagingDirectory);
-        } catch (error) {
-            console.error(error);
-            process.exit(3);
-        }
-
-        // Ensure that we have an S3 bucket to upload Lambdas to (unless dry run)
-        if (!program.dryRun) {
-            console.log(`Making sure S3 bucket "${lambdasBucket}" exists`);
-            const S3 = new AWS.S3({apiVersion: '2006-03-01'});
-            const request = S3.createBucket({
-                Bucket: lambdasBucket,
-            });
-
-            try {
-                sync.await(request.send(sync.defer()));
-            } catch (error) {
-                console.error(error);
-                process.exit(4);
-            }
-
-            console.log('\tDone\n'.green);
-        }
-
-        try {
-            let stackCF = fsx.readJSONFileSync(path.join(workingDirectory, 'templates', 'cf.json'));
-
-            // Project name parameter can be set (along with stage)
-            stackCF["Parameters"]["aaProjectName"] = {
-                "Type": "String",
-                "Default": program.projectName,
-                "AllowedValues": [
-                    program.projectName
-                ]
-            };
-
-            // If there are additional Lamdba policies, add those
-            let policyFile = path.join(root, 'lambda_policies.json');
-            if (fsx.fileExists(policyFile)) {
-                let policies = [].concat(fsx.readJSONFileSync(policyFile));
-                let current = stackCF["Resources"]["IamPolicyLambda"]["Properties"]["PolicyDocument"]["Statement"];
-                stackCF["Resources"]["IamPolicyLambda"]["Properties"]["PolicyDocument"]["Statement"] = current.concat(policies);
-            }
-
-            // Store the partially completed stack formation file
-            fs.writeFileSync(deploymentFormation, JSON.stringify(stackCF));
-
-            // Check if there is an API definition
-            let api;
-            let apiFile = path.join(root, 'api.json');
-            if (fsx.fileExists(apiFile)) {
-                // Copy the API file to staging
-                fs.writeFileSync(deploymentAPI, fs.readFileSync(apiFile));
-                api = deploymentAPI;
-            }
-
-            callback(null, {
-                directories: {
-                    root: root,
-                    working: workingDirectory,
-                    staging: stagingDirectory,
-                    lambdas: lambdasDirectory
-                },
-                cloudFormation: {
-                    stackName: `${program.projectName}-${program.stage}`,
-                    deployment: deploymentFormation
-                },
-                api: {
-                    deployment: api,
-                    importer: path.join(workingDirectory, 'aws-apigateway-importer')
-                },
-                bucket: {
-                    name: lambdasBucket,
-                    directory: lambdasBucketDirectory
+    async.series(
+        [
+            function(callback) {
+                // Check dependencies
+                try {
+                    checkDependencies();
+                    callback();
+                } catch (error) {
+                    callback(error);
                 }
-            });
-        } catch (error) {
-            console.error("Failed to load default templates".red, error);
-            callback(error, null);
-        }
-    });
+            },
+
+            function(callback) {
+                // Make sure there are Lambda functions, otherwise abort
+                if (!fsx.directoryExists(lambdasDirectory)) {
+                    return callback(new Error('Lambdas directory does not exist'));
+                }
+
+                // Create the staging directory (wipe any old one)
+                try {
+                    fsx.recreateDirectory(stagingDirectory);
+                } catch (error) {
+                    return callback(error);
+                }
+
+                // Check if there is an API definition
+                let api;
+                let apiFile = path.join(root, 'api.json');
+                if (fsx.fileExists(apiFile)) {
+                    // Copy the API file to staging
+                    fs.writeFileSync(deploymentAPI, fs.readFileSync(apiFile));
+                    api = deploymentAPI;
+                }
+
+                callback(null, {
+                    directories: {
+                        root: root,
+                        working: workingDirectory,
+                        staging: stagingDirectory,
+                        lambdas: lambdasDirectory
+                    },
+                    cloudFormation: {
+                        stackName: `${program.projectName}-${program.stage}`,
+                        deployment: deploymentFormation
+                    },
+                    api: {
+                        deployment: api,
+                        importer: path.join(workingDirectory, 'aws-apigateway-importer')
+                    },
+                    bucket: {
+                        name: lambdasBucket,
+                        directory: lambdasBucketDirectory
+                    }
+                });
+            },
+
+            function(callback) {
+                // Ensure that we have an S3 bucket to upload Lambdas to (unless dry run)
+                if (!program.dryRun) {
+                    console.log(`Making sure S3 bucket "${lambdasBucket}" exists`);
+                    const S3 = new AWS.S3({apiVersion: '2006-03-01'});
+                    const request = S3.createBucket({
+                        Bucket: lambdasBucket,
+                    });
+
+                    request.send(function(err, result) {
+                        console.log('\tDone\n'.green);
+                        callback(err, result);
+                    });
+                } else {
+                    callback();
+                }
+            },
+
+            function(callback) {
+                try {
+                    // Prime the CF template
+                    let stackCF = fsx.readJSONFileSync(path.join(workingDirectory, 'templates', 'cf.json'));
+
+                    // Project name parameter can be set (along with stage)
+                    stackCF["Parameters"]["aaProjectName"] = {
+                        "Type": "String",
+                        "Default": program.projectName,
+                        "AllowedValues": [
+                            program.projectName
+                        ]
+                    };
+
+                    // If there are additional Lamdba policies, add those
+                    let policyFile = path.join(root, 'lambda_policies.json');
+                    if (fsx.fileExists(policyFile)) {
+                        let policies = [].concat(fsx.readJSONFileSync(policyFile));
+                        let current = stackCF["Resources"]["IamPolicyLambda"]["Properties"]["PolicyDocument"]["Statement"];
+                        stackCF["Resources"]["IamPolicyLambda"]["Properties"]["PolicyDocument"]["Statement"] = current.concat(policies);
+                    }
+
+                    // Store the partially completed stack formation file
+                    fs.writeFileSync(deploymentFormation, JSON.stringify(stackCF));
+
+                    callback();
+                } catch (error) {
+                    console.error("Failed to load default templates".red, error);
+                    callback(error);
+                }
+            }
+        ],
+
+        function(err, results) {
+            callback(err, results[1]);
+        });
 };
