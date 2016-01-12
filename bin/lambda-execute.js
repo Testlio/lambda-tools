d0e7f7b242bdabaf74e1be5331090fb494990c4a
Henri Normak <henri.normak@gmail.com>
2016-01-12 16:52:53 +0200

    Add a simple execute script for running a single lambda function
    This uses the same runtime logic as the service simulation uses, which is excellent as it hides environment and allows testing with a single event file.

---
+++ bin/lambda-execute.js
@@ -0,0 +1,56 @@
+"use strict";
+
+require('colors');
+
+const fsx = require('../lib/helpers/fs-additions');
+const program = require('commander');
+const parseEnvironment = require('../lib/helpers/environment-parser.js');
+const path = require('path');
+
+const Execution = require('../lib/run/execution');
+
+//
+//  Program specification
+//
+
+program
+    .option('-f, --file <file>', 'Path to the Lambda function entry point')
+    .option('-e, --event <file>', 'Path to the event JSON file, defaults to \'./event.json\'')
+    .option('--environment <env>', 'Environment Variables to embed as key-value pairs', parseEnvironment)
+    .option('-t, --timeout <timeout>', 'Timeout value for the Lambda function')
+    .parse(process.argv);
+
+// Determine our target directory
+program.directory = process.cwd();
+
+// Default values for params
+program.environment = program.environment || {};
+program.timeout = program.timeout || 6;
+program.file = path.resolve(program.directory, program.file);
+program.event = path.resolve(program.directory, program.event);
+this.program = program;
+
+const event = fsx.readJSONFileSync(program.event);
+const context = {
+    functionName: path.basename(program.file),
+    invokedFunctionArn: '$LATEST',
+    memoryLimitInMB: '1024',
+    timeout: program.timeout
+};
+
+console.log('Executing Lambda function'.bold.green);
+console.log('\tWith event:');
+console.log(JSON.stringify(event, null, '\t').split('\n').join('\n\t'), '\n');
+console.log('\t--'.gray);
+
+let promise = Execution.bind(this)(program.file, event, context).next().value;
+
+promise.then(function(result) {
+    console.log('\t--'.gray);
+    console.log('Lambda executed');
+    console.log('\t--'.gray);
+    console.log('Result:', result);
+}).catch(function(error) {
+    console.log('Lambda failed'.bold.red);
+    console.error(error);
+});
--- bin/lambda.js
+++ bin/lambda.js
@@ -9,4 +9,5 @@ program
   .command('deploy', 'deploy Lambda based microservice to AWS')
   .command('deploy-single', 'deploy a single Lambda function to AWS')
   .command('run', 'run Lambda based microservice locally', { isDefault: true })
+  .command('execute', 'execute a single Lambda function locally')
   .parse(process.argv);
