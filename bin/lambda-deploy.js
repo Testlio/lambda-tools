caba49f66e2f2ef59b98d01989fe39f6c3fc3c72
Henri Normak <henri.normak@gmail.com>
2016-04-22 09:32:43 +0300

    Improve --help of deploy command

--- bin/lambda-deploy.js
+++ bin/lambda-deploy.js
@@ -34,8 +34,26 @@ program
     .option('--exclude <list>', 'Packages to exclude from bundling', function(val) { return val.split(','); })
     .option('-o, --optimization <level>', 'Optimization level to use, valid values are 0-1', parseInt, 1)
     .option('--clean', 'Force a clean build where cached bundles are not used')
-    .option('--no-color', 'Turn off ANSI coloring in output')
-    .parse(process.argv);
+    .option('--no-color', 'Turn off ANSI coloring in output');
+
+program.on('--help', function() {
+    console.log('  Examples:');
+    console.log();
+    console.log('    Generate deployment files (in staging directory), but don\'t actually deploy');
+    console.log('    $ lambda deploy --dry-run');
+    console.log();
+    console.log('    Deploy to \'prod\' stage with NODE_ENV set to \'production\' and FOO set to \'bar\'')
+    console.log('    $ lambda deploy -s prod -e NODE_ENV=production,FOO=bar');
+    console.log();
+    console.log('    Deploy to default (dev) stage excluding \'example\' package from the bundle (included in the ZIP separately)');
+    console.log('    $ lambda deploy --exclude example');
+    console.log();
+    console.log('    Deploy to default stage, ignoring cached bundles and disabling minification');
+    console.log('    $ lambda deploy --clean --optimization 0');
+    console.log();
+});
+
+program.parse(process.argv);
 
 //
 // Configure program
