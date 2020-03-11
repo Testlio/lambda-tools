c32ef444094878a37a3eabfb1b394da75af71696
Matten Mätlik <matten.matlik@gmail.com>
2020-03-11 15:49:24 +0200

    GREY-1780: Add support for resource suffix via lambda setup

--- bin/lambda-setup.js
+++ bin/lambda-setup.js
@@ -14,11 +14,12 @@ const createResources = require('../lib/setup/create-cf-resources.js');
 program
     .option('-r, --region <string>', 'Region to setup in, if not set otherwise, defaults to \'us-east-1\'')
     .option('-p, --resource-prefix <string>', 'Prefix to use with all lambda-tools created AWS resources, defaults to \'\' (empty string)')
+    .option('-s, --resource-suffix <string>', 'Suffix to use with all lambda-tools created AWS resources, defaults to \'\' (empty string)')
     .option('--no-color', 'Turn off ANSI coloring in output')
     .parse(process.argv);
 
 chalk.enabled = program.color;
 
 logger.task(`Setting up ${chalk.underline('lambda-tools')}`, function(resolve, reject) {
-    createResources(program.region, program.resourcePrefix).then(resolve, reject);
+    createResources(program.region, program.resourcePrefix, program.resourceSuffix).then(resolve, reject);
 });
